08862642a755ef67165118e581d1f14f7a21df90
Luis Timaure <luistimaure1204@gmail.com>
2026-04-18 00:32:37 +0000

    Ajuste de funcion generica Se adiciono funcion generica para generar numero aleatoreos de un arreglo

--- src/app.js
+++ src/app.js
@@ -5,7 +5,22 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+function generateRandom(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+  let valWho = generateRandom(who);
+  let valAction = generateRandom(action);
+  let valwhat = generateRandom(what);
+  let valwhen = generateRandom(when);
+  document.querySelector("#excuse").innerHTML = valWho + ' '+ valAction+ ' '+ valwhat+' '+ valwhen;
+  
 };
+
+
